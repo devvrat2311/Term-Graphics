47b6a5d290042e5a9564847c57e56b918039a650
deba <debabrata1729@gmail.com>
2023-11-29 17:57:33 +0530

    [+] - input functionality with functions like handleMovement(), [+] - console at the bottom of the board to display strings as prompts in game(later stages)

--- include/game.h
+++ include/game.h
@@ -2,13 +2,23 @@
 #define GAME_H
 
 #include "grid.h"
+#include<string>
 #include<map>
 
 class Game {
-public:
+private:
+		bool Running;
     Board gameBoard; //initializes the gameBoard used for the Game instance
+		std::string log;
+		int positionX;
+		int positionY;
+
+public:
     Game(); //constructor for the gameBoard  
     void display(); //calls functions from the gameBoard to finally display the game
+		void gameRun();
+		void console();
+		void handleMovement(char key);
 
 };
 
--- src/game.cpp
+++ src/game.cpp
@@ -1,18 +1,83 @@
 #include"game.h"
+#include "input.h"
 #include<iostream>
 
 
-Game::Game() : gameBoard()
+
+Game::Game() : gameBoard(87,31)
 {
-    std::cout << "gameboard initialized. . . beep boop" << '\n';
+		positionX = 0;
+		positionY = -3;
+		Running = true;
+    std::cout << "gameboard constructed!!. . . beep boop" << '\n';
+		log = "Welcome to Game: 󰜃";
 }
 
 void Game::display() {
-  std::cout << "calling from Game display \n"; 
-  gameBoard.renderInitialSetup();
-  gameBoard.drawBoard();
-  std::cout << gameBoard.count << std::endl;
-	
+		gameBoard.renderInitialSetup();
+		gameBoard.setCharAt(positionX,positionY,'x');
+		gameBoard.renderBox(2,2);
+		gameBoard.drawBoard();
+		console();
 }
 
+//system("clear") being called here!
+void Game::gameRun() {
+	while(Running) {
+		system("clear");
+		display();
+		std::cout << "\n Press q to quit game! \n";
+
+		char key = getch(); //getch() is the key to using system("clear") 
+												//effevtively, i think.
+
+		handleMovement(key);
+		if(key != 'q'){
+			log = "Last key pressed was:";
+			log += key;
+		}
+
+		if(key == 'q') {
+			log="Quit\n";
+			Running = false;
+		}
+
+	}
+}
+
+void Game::console() {
+	int Ylim = gameBoard.Yaxis;
+	int Xlim = gameBoard.Xaxis;
+	for(int i = 0; i < 3; i++) {
+		for(int j = 0; j < Xlim; j++) {
+			if(i == 1){
+				if(j == 1) 
+					std::cout << log;
+			}
+			else{
+				std::cout << "+";
+			}
+		}
+		std::cout << '\n';
+	}
+}
+
+void Game::handleMovement(char key) {
+	switch(key) {
+		case 'k':
+			positionY++;
+			break;
+		case 'j':
+			positionY--;
+			break;
+		case 'l':
+			positionX++;
+			break;
+		case 'h':
+			positionX--;
+			break;
+		default:
+			break;
+	}
+}
 
